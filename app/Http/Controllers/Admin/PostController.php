6541e96ffb5dd7cfb4c5b5e0c94cd45b87809738
Sergio <sergio.cbmostoles@gmail.com>
2021-03-22 17:20:27 +0100

    Amazon s3

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -51,7 +51,8 @@ class PostController extends Controller
         $post = Post::create($request->all());
 
         if($request->file('file')){
-            $url = Storage::put('posts', $request->file('file'));
+            $url = Storage::disk('s3')->put('posts', $request->file('file'));
+
 
             $post->image()->create([
                 'url' => $url
